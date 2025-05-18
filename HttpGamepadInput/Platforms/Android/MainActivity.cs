9837d36c5b94c60afe2e7ba493fbb3945d2f7c15
Michael S. Kataev <lpestlname@gmail.com>
2025-05-19 00:22:40 +0300

    Updated android settings

--- HttpGamepadInput/Platforms/Android/MainActivity.cs
+++ HttpGamepadInput/Platforms/Android/MainActivity.cs
@@ -6,7 +6,8 @@ namespace HttpGamepadInput;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop,
     ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode |
-                           ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+                           ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density,
+    ScreenOrientation = ScreenOrientation.Landscape)]
 public class MainActivity : MauiAppCompatActivity
 {
 }
