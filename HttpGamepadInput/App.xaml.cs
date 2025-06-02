305b52da9762a10324afdd0e4b1f98bd166a6e7d
Michael S. Kataev <lpestlname@gmail.com>
2025-06-02 15:18:27 +0300

    Added handle for UnhandledExceptions

--- HttpGamepadInput/App.xaml.cs
+++ HttpGamepadInput/App.xaml.cs
@@ -1,14 +1,40 @@
-﻿namespace HttpGamepadInput;
+﻿using System.Diagnostics;
+
+namespace HttpGamepadInput;
 
 public partial class App : Application
 {
     public App()
     {
         InitializeComponent();
+        
+        AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
+        TaskScheduler.UnobservedTaskException += OnUnobservedTaskException;
+
     }
 
     protected override Window CreateWindow(IActivationState? activationState)
     {
         return new Window(new AppShell());
+    }
+    
+    private void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+    {
+        Exception? ex = e.ExceptionObject as Exception;
+        Debug.WriteLine($"[UNHANDLED] {ex?.Message}");
+        MainThread.BeginInvokeOnMainThread(async () =>
+        {
+            await Application.Current?.MainPage?.DisplayAlert("Error", $"[UNHANDLED] {ex?.Message}", "OK");
+        });
+    }
+
+    private void OnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+    {
+        Debug.WriteLine($"[TASK ERROR] {e.Exception.Message}");
+        e.SetObserved();
+        MainThread.BeginInvokeOnMainThread(async () =>
+        {
+            await Application.Current?.MainPage?.DisplayAlert("Task error", $"[TASK ERROR] {e.Exception.Message}", "OK");
+        });
     }
 }
